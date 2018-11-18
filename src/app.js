29029b078a19590ef8a859751af06fd09c866de9
Tyler Creller <tylercreller@gmail.com>
2018-11-18 14:50:23 -0500

    Moved counter app to playground

--- src/app.js
+++ src/app.js
@@ -17,25 +17,5 @@ const template = (
     </div>
 );
 
-let count = 0;
-const addOne = () => {
-    console.log('addOne');
-}
-const minusOne = () => {
-    console.log('minusOne');
-}
-const reset = () => {
-    console.log('reset');
-}
-const templateTwo = (
-    <div>
-        <h1>Count: {count}</h1>
-        <button onClick={addOne}>+1</button>
-        <button onClick={minusOne}>-1</button>
-        <button onClick={reset}>reset</button>
-    </div>
-);
-console.log(templateTwo);
 const appRoot = document.getElementById('app');
-    
-ReactDOM.render(templateTwo, appRoot);
+ReactDOM.render(template, appRoot);
