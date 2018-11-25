809d51cbef5cfe250ade32ffda709a64f25f30df
Tyler Creller <tylercreller@gmail.com>
2018-11-25 16:46:40 -0500

    Stateful visibility toggle

--- src/playground/build-it-visible.js
+++ src/playground/build-it-visible.js
@@ -1,26 +1,58 @@
-const appRoot = document.getElementById('app');
-let showDetails = false;
+class VisibilityToggle extends React.Component {
+	constructor(props) {
+		super(props);
+		this.handleToggleVisibility = this.handleToggleVisibility.bind(this);
+		this.state = {
+			visible: false
+		};
+	}
 
-const toggleDetails = () => {
-    showDetails = !showDetails;
-    render();
-};
+	render() {
+		return (
+			<div>
+				<h1>Visibility Toggle</h1>
+				<button onClick={this.handleToggleVisibility}>
+					{!this.state.visible ? 'Show Details' : 'Hide Details'}
+				</button>
+				{this.state.visible && <p>Here are the details</p>}
+			</div>
+		);
+	}
 
-const getButtonName = () => {
-    return !showDetails ? 'Show Details' : 'Hide Details'
+	handleToggleVisibility() {
+		this.setState(prevState => {
+			return {
+				visible: !prevState.visible
+			};
+		});
+	}
 }
 
-const render = () => {
-    const template = (
-        <div>
-            <h1>Visibility Toggle</h1>
-            <button onClick={toggleDetails}>{getButtonName()}</button>
-            {showDetails && (
-                <p>Here are the details.</p>
-            )}
-        </div>
-    );
-    ReactDOM.render(template, appRoot);
-};
+ReactDOM.render(<VisibilityToggle />, document.getElementById('app'));
 
-render();
+// const appRoot = document.getElementById('app');
+// let showDetails = false;
+
+// const toggleDetails = () => {
+//     showDetails = !showDetails;
+//     render();
+// };
+
+// const getButtonName = () => {
+//     return !showDetails ? 'Show Details' : 'Hide Details'
+// }
+
+// const render = () => {
+//     const template = (
+//         <div>
+//             <h1>Visibility Toggle</h1>
+//             <button onClick={toggleDetails}>{getButtonName()}</button>
+//             {showDetails && (
+//                 <p>Here are the details.</p>
+//             )}
+//         </div>
+//     );
+//     ReactDOM.render(template, appRoot);
+// };
+
+// render();
