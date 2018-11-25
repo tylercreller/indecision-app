897fd835bcaffeaa6ba875e65f9e6f1d27f244e2
Tyler Creller <tylercreller@gmail.com>
2018-11-25 18:05:58 -0500

    Remove comments

--- src/playground/build-it-visible.js
+++ src/playground/build-it-visible.js
@@ -29,30 +29,3 @@ class VisibilityToggle extends React.Component {
 }
 
 ReactDOM.render(<VisibilityToggle />, document.getElementById('app'));
-
-// const appRoot = document.getElementById('app');
-// let showDetails = false;
-
-// const toggleDetails = () => {
-//     showDetails = !showDetails;
-//     render();
-// };
-
-// const getButtonName = () => {
-//     return !showDetails ? 'Show Details' : 'Hide Details'
-// }
-
-// const render = () => {
-//     const template = (
-//         <div>
-//             <h1>Visibility Toggle</h1>
-//             <button onClick={toggleDetails}>{getButtonName()}</button>
-//             {showDetails && (
-//                 <p>Here are the details.</p>
-//             )}
-//         </div>
-//     );
-//     ReactDOM.render(template, appRoot);
-// };
-
-// render();
