690076a7d95b972f6696532d6bd17df8654e10d4
Tyler Creller <tylercreller@gmail.com>
2018-11-26 18:18:09 -0500

    Stateless functional components

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ class IndecisionApp extends React.Component {
 		this.handlePick = this.handlePick.bind(this);
 		this.handleAddOption = this.handleAddOption.bind(this);
 		this.state = {
-			options: []
+			options: props.options
 		};
 	}
 	handleDeleteOptions() {
@@ -34,12 +34,11 @@ class IndecisionApp extends React.Component {
 		});
 	}
 	render() {
-		const title = 'Indecision App';
 		const subtitle = 'Put your life in the hands of a computer';
 
 		return (
 			<div>
-				<Header title={title} subtitle={subtitle} />
+				<Header subtitle={subtitle} />
 				<Action
 					hasOptions={this.state.options.length > 0}
 					handlePick={this.handlePick}
@@ -54,50 +53,47 @@ class IndecisionApp extends React.Component {
 	}
 }
 
-class Header extends React.Component {
-	render() {
-		return (
-			<div>
-				<h1>{this.props.title}</h1>
-				<h2>{this.props.subtitle}</h2>
-			</div>
-		);
-	}
-}
+IndecisionApp.defaultProps = {
+	options: []
+};
 
-class Action extends React.Component {
-	render() {
-		return (
-			<div>
-				<button
-					onClick={this.props.handlePick}
-					disabled={!this.props.hasOptions}
-				>
-					What Should I do?
-				</button>
-			</div>
-		);
-	}
-}
+const Header = props => {
+	return (
+		<div>
+			<h1>{props.title}</h1>
+			<h2>{props.subtitle}</h2>
+		</div>
+	);
+};
 
-class Option extends React.Component {
-	render() {
-		return <div>{this.props.optionText}</div>;
-	}
-}
+Header.defaultProps = {
+	title: 'Indecision'
+};
 
-class Options extends React.Component {
-	render() {
-		return (
-			<div>
-				<button onClick={this.props.handleDeleteOptions}>Remove All</button>
-				{this.props.options.map(option => (
-					<Option key={option} optionText={option} />
-				))}
-			</div>
-		);
-	}
-}
+const Action = props => {
+	return (
+		<div>
+			<button onClick={props.handlePick} disabled={!props.hasOptions}>
+				What Should I do?
+			</button>
+		</div>
+	);
+};
+
+const Option = props => {
+	return <div>{props.optionText}</div>;
+};
+
+const Options = props => {
+	return (
+		<div>
+			<button onClick={props.handleDeleteOptions}>Remove All</button>
+			{props.options.map(option => (
+				<Option key={option} optionText={option} />
+			))}
+		</div>
+	);
+};
 
 class AddOption extends React.Component {
 	constructor(props) {
--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -5,7 +5,7 @@ class Counter extends React.Component {
 		this.handleMinusOne = this.handleMinusOne.bind(this);
 		this.handleReset = this.handleReset.bind(this);
 		this.state = {
-			count: 0
+			count: props.count
 		};
 	}
 
@@ -45,7 +45,11 @@ class Counter extends React.Component {
 	}
 }
 
-ReactDOM.render(<Counter />, document.getElementById('app'));
+Counter.defaultProps = {
+	count: 1
+};
+
+ReactDOM.render(<Counter count={4} />, document.getElementById('app'));
 
 // let count = 0;
 // const addOne = () => {
